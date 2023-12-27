7e4fe5ec15b2a5105e8fecc6e0b67913097b5c72
LeoAzevedo59 <leofrancoazevedo@gmail.com>
2023-12-26 23:07:41 -0300

    feat: style restyle in component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,30 +1,29 @@
 import React from 'react';
-import {TouchableOpacity} from 'react-native';
 import {Text} from '../Text/Text';
-import {useTheme} from '@shopify/restyle';
-import {Theme} from '../../theme/theme';
+import {Box} from '../Box/Box';
+import {ActivityIndicator} from 'react-native';
 
 interface ButtonProps {
   title: string;
+  loading?: boolean;
 }
 
-export function Button({title}: ButtonProps) {
-  const {colors} = useTheme<Theme>();
-
+export function Button({title, loading}: ButtonProps) {
   return (
     <>
-      <TouchableOpacity
-        style={{
-          paddingHorizontal: 20,
-          paddingVertical: 14,
-          backgroundColor: colors.carrotSecondary,
-          alignItems: 'center',
-          borderRadius: 16,
-        }}>
-        <Text bold style={{color: '#fff'}}>
-          {title}
-        </Text>
-      </TouchableOpacity>
+      <Box
+        alignItems="center"
+        justifyContent="center"
+        height={50}
+        backgroundColor="carrotSecondary">
+        {loading ? (
+          <ActivityIndicator color={'#fff'} />
+        ) : (
+          <Text bold style={{color: '#fff'}}>
+            {title}
+          </Text>
+        )}
+      </Box>
     </>
   );
 }
