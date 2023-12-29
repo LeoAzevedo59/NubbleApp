0681db9989d6f415202f81614292722b5ce53fec
LeoAzevedo59 <leofrancoazevedo@gmail.com>
2023-12-29 13:37:06 -0300

    feat: add first screen with my components

--- App.tsx
+++ App.tsx
@@ -1,29 +1,43 @@
 import React from 'react';
 
-import {SafeAreaView} from 'react-native';
-import {Text} from './src/components/Text/Text';
-import {Button} from './src/components/Button/Button';
+import {SafeAreaView, TextInput, View} from 'react-native';
 import {ThemeProvider} from '@shopify/restyle';
 import {theme} from './src/theme/theme';
+import {Text} from './src/components/Text/Text';
 import {Box} from './src/components/Box/Box';
-import {Icon} from './src/components/Icon/Icon';
+import {Button} from './src/components/Button/Button';
 
 export default function App(): React.JSX.Element {
   return (
     <ThemeProvider theme={theme}>
       <SafeAreaView>
-        <Box paddingHorizontal="s12">
-          <Text preset="headingLarge">Coff Stack</Text>
-          <Button
-            disabled
-            preset="outline"
-            title="Created"
-            marginBottom="s24"
-          />
-          <Button loading title="Created" />
-          <Icon name="eyeOn" />
-          <Icon name="eyeOn" />
-        </Box>
+        <View style={{paddingHorizontal: 24}}>
+          <Text marginBottom="s8" preset="headingLarge">
+            Olá
+          </Text>
+          <Text marginBottom="s40" preset="paragraphLarge">
+            Digite seu e-mail e senha para entrar
+          </Text>
+          <Box mb="s20">
+            <TextInput
+              placeholder="Digite seu e-mail"
+              style={{borderWidth: 1, height: 50}}
+            />
+          </Box>
+          <Box mb="s20">
+            <TextInput
+              placeholder="Digite sua senha"
+              style={{borderWidth: 1, height: 50}}
+            />
+          </Box>
+
+          <Text mt="s8" preset="paragraphSmall" bold color="greenPrimary">
+            Esqueci minha senha
+          </Text>
+
+          <Button mt="s48" title="Entrar" />
+          <Button mt="s12" preset="outline" title="Criar uma conta" />
+        </View>
       </SafeAreaView>
     </ThemeProvider>
   );
--- src/components/ActivityIndicator/ActivityIndicator.tsx
+++ src/components/ActivityIndicator/ActivityIndicator.tsx
@@ -4,14 +4,14 @@ import {
   ActivityIndicatorProps as RNActivityIndicatorProps,
 } from 'react-native';
 import {ThemeColors} from '../../theme/theme';
-import useAppTheme from '../../hooks/useAppTheme';
+import {useAppTheme} from '../../hooks/useAppTheme';
 
 interface ActivityIndicatorProps
   extends Omit<RNActivityIndicatorProps, 'color'> {
   color: ThemeColors;
 }
 
-export default function ActivityIndicator({color}: ActivityIndicatorProps) {
+export function ActivityIndicator({color}: ActivityIndicatorProps) {
   const {colors} = useAppTheme();
 
   return <RNActivityIndicator color={colors[color]} />;
