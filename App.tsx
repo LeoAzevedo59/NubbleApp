5895c97b7dcfb26e3a169dd5a19d5dba8f6220c2
LeoAzevedo59 <leofrancoazevedo@gmail.com>
2023-12-31 12:34:06 -0300

    feat: add lib

--- App.tsx
+++ App.tsx
@@ -2,15 +2,14 @@ import React from 'react';
 
 import {ThemeProvider} from '@shopify/restyle';
 import {theme} from './src/theme/theme';
-
 import {SafeAreaProvider} from 'react-native-safe-area-context';
-import {SignUpScreen} from './src/screens/auth/SignUpScreen/SignUpScreen';
+import {Routes} from './src/routes/Routes';
 
 export default function App(): React.JSX.Element {
   return (
     <SafeAreaProvider>
       <ThemeProvider theme={theme}>
-        <SignUpScreen />
+        <Routes />
       </ThemeProvider>
     </SafeAreaProvider>
   );
