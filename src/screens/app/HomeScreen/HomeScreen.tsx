fd3697751243a23c9964df673654c35de5ad8626
LeoAzevedo59 <leofrancoazevedo@gmail.com>
2024-01-10 19:39:17 -0300

    feat: test add component post

--- src/screens/app/HomeScreen/HomeScreen.tsx
+++ src/screens/app/HomeScreen/HomeScreen.tsx
@@ -1,17 +1,44 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {Dimensions, FlatList, Image, ListRenderItemInfo} from 'react-native';
 
-import {Button, Screen, Text} from '@components';
+import {Post, postService} from '@domain';
+
+import {Box, Screen, Text} from '@components';
 import {AppTabScreenProps} from '@routes';
 
-export function HomeScreen({navigation}: AppTabScreenProps<'HomeScreen'>) {
-  function navigationToSettings() {
-    navigation.navigate('SettingsScreen');
+export function HomeScreen({}: AppTabScreenProps<'HomeScreen'>) {
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  useEffect(() => {
+    postService.getList().then(list => setPosts(list));
+  }, []);
+
+  function renderItem({item}: ListRenderItemInfo<Post>) {
+    return (
+      <Box marginBottom="s24">
+        <Box flexDirection="row">
+          <Image
+            source={{uri: item.author.profileURL}}
+            style={{width: 32, height: 32}}
+          />
+          <Text>{item.author.name}</Text>
+        </Box>
+        <Image
+          source={{uri: item.imageURL}}
+          resizeMode="cover"
+          style={{width: Dimensions.get('screen').width, height: 300}}
+        />
+      </Box>
+    );
   }
 
   return (
     <Screen>
-      <Text preset="headingLarge">Home screen</Text>
-      <Button onPress={navigationToSettings} title="Settings" />
+      <FlatList
+        data={posts}
+        keyExtractor={post => post.id}
+        renderItem={renderItem}
+      />
     </Screen>
   );
 }
